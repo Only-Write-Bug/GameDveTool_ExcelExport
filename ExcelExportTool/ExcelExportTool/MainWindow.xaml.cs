dee75e88fc4d45c9e92334f4cfe6eccd107edf5b
KKKmoji <qwer123456qin@163.com>
2024-09-02 00:18:23 +0800

    [feature] 增加可选数据记录存储

--- ExcelExportTool/ExcelExportTool/MainWindow.xaml.cs
+++ ExcelExportTool/ExcelExportTool/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Text.Json;
 using System.Windows;
 using Microsoft.Win32;
 using Path = System.IO.Path;
@@ -11,18 +12,21 @@ namespace ExcelExportTool;
 public partial class MainWindow : Window
 {
     private const string _PRIVATE_DATA_FOLDER_NAME = "_data";
+    private const string _OPTIONAL_DATA_FILE_NAME = "optional_data.json";
     
     private LogWindow _curLogWindow = null;
     
-    private string _toolPrivateDataPath= null;
-    private string _curExcelsPath = null;
-    private string _curXMLsPath = null; 
+    private string _toolPrivateDataFolderPath = null;
+    
+    private OptionalData _optionalData = null;
 
     public MainWindow()
     {
         InitializeComponent();
 
         Check_CompletionToolPrivateDataFolder();
+        _optionalData = LoadOptionalData();
+        
         InitExcelPathEditorBtn();
         InitXMLPathEditorBtn();
         InitStartBtn();
@@ -30,24 +34,26 @@ public partial class MainWindow : Window
 
     private void InitExcelPathEditorBtn()
     {
+        ExcelPathTextBox.Text = _optionalData.curExcelsFolderPath;
         ExcelPathEditorButton.Click += (sender, args) =>
         {
             var folderDialog = new OpenFolderDialog();
             if (folderDialog.ShowDialog() == true)
             {
-                _curExcelsPath = ExcelPathTextBox.Text = folderDialog.FolderName;
+                _optionalData.curExcelsFolderPath = ExcelPathTextBox.Text = folderDialog.FolderName;
             }
         };
     }
 
     private void InitXMLPathEditorBtn()
     {
+        XMLPathTextBox.Text = _optionalData.curXMLsFolderPath;
         XmlPathEditorButton.Click += (sender, args) =>
         {
             var folderDialog = new OpenFolderDialog();
             if (folderDialog.ShowDialog() == true)
             {
-                _curXMLsPath = XMLPathTextBox.Text = folderDialog.FolderName;
+                _optionalData.curXMLsFolderPath = XMLPathTextBox.Text = folderDialog.FolderName;
             }
         };
     }
@@ -58,9 +64,13 @@ public partial class MainWindow : Window
         {
             var logWindow = new LogWindow();
             logWindow.Show();
+            
             _curLogWindow?.Close();
             _curLogWindow = logWindow;
             _curLogWindow.AddLog(FinishResults.Success, "Tool is running!!!");
+            
+            SaveOptionalData();
+            
             if (!CheckPathConfig())
             {
                 _curLogWindow.AddLog(FinishResults.Failure, "Work failed, please fix bug by log, try again");
@@ -75,12 +85,12 @@ public partial class MainWindow : Window
     /// <returns></returns>
     private bool CheckPathConfig()
     {
-        if (string.IsNullOrEmpty(_curExcelsPath) || Directory.Exists(_curExcelsPath))
+        if (string.IsNullOrEmpty(_optionalData.curExcelsFolderPath) || Directory.Exists(_optionalData.curExcelsFolderPath))
         {
             _curLogWindow.AddLog(FinishResults.Failure, "Excel path is Error");
             return false;
         }
-        if (string.IsNullOrEmpty(_curXMLsPath) || Directory.Exists(_curXMLsPath))
+        if (string.IsNullOrEmpty(_optionalData.curXMLsFolderPath) || Directory.Exists(_optionalData.curXMLsFolderPath))
         {
             _curLogWindow.AddLog(FinishResults.Failure, "XML path is Error");
             return false;
@@ -96,12 +106,50 @@ public partial class MainWindow : Window
     /// </summary>
     private void Check_CompletionToolPrivateDataFolder()
     {
-        _toolPrivateDataPath = Path.Combine(Directory.GetCurrentDirectory(), _PRIVATE_DATA_FOLDER_NAME);
-        if (!Directory.Exists(_toolPrivateDataPath))
+        _toolPrivateDataFolderPath = Path.Combine(Directory.GetCurrentDirectory(), _PRIVATE_DATA_FOLDER_NAME);
+        if (!Directory.Exists(_toolPrivateDataFolderPath))
         {
-            Directory.CreateDirectory(_toolPrivateDataPath);
-            var directoryInfo = new DirectoryInfo(_toolPrivateDataPath);
+            Directory.CreateDirectory(_toolPrivateDataFolderPath);
+            var directoryInfo = new DirectoryInfo(_toolPrivateDataFolderPath);
             directoryInfo.Attributes |= FileAttributes.Hidden;
         }
+    }
+
+    /// <summary>
+    /// 可选数据路径生成工厂
+    /// </summary>
+    /// <returns></returns>
+    private string OptionalDataFileFactory()
+    {
+        if (string.IsNullOrEmpty(_toolPrivateDataFolderPath) || !Directory.Exists(_toolPrivateDataFolderPath))
+            Check_CompletionToolPrivateDataFolder();
+        
+        return Path.Combine(_toolPrivateDataFolderPath, _OPTIONAL_DATA_FILE_NAME);
+    }
+
+    /// <summary>
+    /// 保存可选数据
+    /// </summary>
+    private void SaveOptionalData()
+    {
+        var dataString = JsonSerializer.Serialize(_optionalData, new JsonSerializerOptions()
+        {
+            WriteIndented = true
+        });
+        File.WriteAllText(OptionalDataFileFactory(), dataString);
+    }
+
+    /// <summary>
+    /// 加载可选数据
+    /// </summary>
+    /// <returns></returns>
+    private OptionalData LoadOptionalData()
+    {
+        if (string.IsNullOrEmpty(_toolPrivateDataFolderPath) || !Directory.Exists(_toolPrivateDataFolderPath) || !File.Exists(OptionalDataFileFactory()))
+            return new OptionalData();
+        
+        var data = JsonSerializer.Deserialize<OptionalData>(File.ReadAllText(OptionalDataFileFactory()));
+        
+        return data ?? new OptionalData();
     }
 }
---
+++ ExcelExportTool/ExcelExportTool/OptionalData.cs
@@ -0,0 +1,7 @@
+namespace ExcelExportTool;
+
+public class OptionalData
+{
+    public string curExcelsFolderPath { get; set; } = "";
+    public string curXMLsFolderPath { get; set; } = "";
+}
